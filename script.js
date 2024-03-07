d8e11021472553a40ab0e80a45a2e22ffa799031
TofuMech <tony.w.wiles@gmail.com>
2024-03-07 11:34:37 -0500

    UI uverhaul
    Over the course of this project I've moved css elements all over the place, and I will certainly continue to do so. However, I managed to remove and condense a lot of css properties and either replace them with javascript, or just manage to trash ones that have no reason of staying. Currently unaware of any issues and will continue to watch the font issues ive been having. I need to find a better way for browsers to load the text style faster, or make a failsafe that makes the page buffer for a few seconds before displaying literally anything.

--- script.js
+++ script.js
@@ -6,31 +6,23 @@ var sunIcon = document.querySelector('#sun');
 var menuBar3 = document.querySelector('#bar3');
 var menuBar2 = document.querySelector('#bar2');
 
-
-
-
-
-
-
 button.addEventListener('click', function () {
   if (!isMoved) {
     box.style.transform = 'translateX(1000px)';
     menuBar3.style.transform = 'translateY(-17px)';
     menuBar2.style.transform = 'translateY(-9px)';
-    
     isMoved = true;
   } else {
     box.style.transform = 'translateX(0)';
     menuBar3.style.transform = 'translateY(0px)';
     menuBar2.style.transform = 'translateY(0px)';
-
     isMoved = false;
   }
 });
 
-// check if a theme preference has already been set
+// Check if a theme preference has already been set
 if (localStorage.getItem('theme')) {
-  // set the active icon and apply the stored theme preference
+  // Set the active icon and apply the stored theme preference
   if (localStorage.getItem('theme') === 'light') {
     sunIcon.classList.add('active');
     applyTheme('light');
@@ -39,17 +31,17 @@ if (localStorage.getItem('theme')) {
     applyTheme('dark');
   }
 } else {
-  // set a default theme preference of 'light' and apply it
+  // Set a default theme preference of 'light' and apply it
   localStorage.setItem('theme', 'light');
   sunIcon.classList.add('active');
   applyTheme('light');
 }
 
-// add event listeners to the moon and sun icons
+// Add event listeners to the moon and sun icons
 moonIcon.addEventListener('click', () => {
-  // store the selected theme preference in local storage
+  // Store the selected theme preference in local storage
   localStorage.setItem('theme', 'dark');
-  // animate the icons and apply the selected theme
+  // Animate the icons and apply the selected theme
   moonIcon.classList.add('active');
   sunIcon.classList.remove('active');
   setTimeout(() => {
@@ -57,50 +49,35 @@ moonIcon.addEventListener('click', () => {
   }, 100);
 });
 sunIcon.addEventListener('click', () => {
-  // store the selected theme preference in local storage
+  // Store the selected theme preference in local storage
   localStorage.setItem('theme', 'light');
-  // animate the icons and apply the selected theme
+  // Animate the icons and apply the selected theme
   sunIcon.classList.add('active');
   moonIcon.classList.remove('active');
   setTimeout(() => {
     applyTheme('light');
   }, 100);
 });
-// function to apply the selected theme
+
+// Function to apply the selected theme
 function applyTheme(theme) {
   if (theme === 'light') {
-    
-    //light theme color attributes
+    // Light theme color attributes
     document.body.style.transition = 'all .1s';
-    document.body.style.backgroundColor = '#c4c8cf'; 
+    document.body.style.backgroundColor = '#c4c8cf';
     document.documentElement.style.setProperty('--text-color', '#8BAAAD');
     document.documentElement.style.setProperty('--button-text', '#F4FFF8');
     document.documentElement.style.setProperty('--button-colorOne', '#D7263D');
-    document.documentElement.style.setProperty('--button-colorTwo', '#4D4847');
-    document.documentElement.style.setProperty('--navbar-color', '#37A1A1 ');
-
+    document.documentElement.style.setProperty('--button-colorTwo', '#2E293A');
+    document.documentElement.style.setProperty('--navbar-color', '#37A1A1');
   } else if (theme === 'dark') {
-    //dark theme color attributes
+    // Dark theme color attributes
     document.body.style.transition = 'all .1s';
     document.body.style.backgroundColor = '#363747';
     document.documentElement.style.setProperty('--text-color', '#4591EE');
-    document.documentElement.style.setProperty('--button-text', '#FFC07D');
+    document.documentElement.style.setProperty('--button-text', '#07192C');
     document.documentElement.style.setProperty('--button-colorOne', '#3a5f7c');
-    document.documentElement.style.setProperty('--button-colorTwo', '#524d3b'); //This should be the darker color of the button
+    document.documentElement.style.setProperty('--button-colorTwo', '#2E293A');
     document.documentElement.style.setProperty('--navbar-color', '#284059');
   }
 }
-function displayRandomText() {
-  var quotes = [
-    "[TAKE YOUR TIME]",
-    "[LOADING..]",
-    "[DRINK WATER]",
-    "[CLEAN YOUR SCREEN]",
-    "[DESTROY THE INTERNET]",
-    "[I GET NO KICK FROM CHAMPAGNE]",
-    "[E̴̲̼͖͉̅N̸̛̝͛̀̈́͗͑͊̃̓̓̈̂̇̃̚͠J̵͎̪͇̭̘̐̈́̂͋̀̀̈̐̈́͊̈̔̈́͑͘͜Ǫ̷͕̗̱͚̭̲̬͇̱̠͉͔̥̞̗̓͋́́̋̔̊̀́͐̈́́̔͑̕Y̸̧̧̛̙̭̰̞̲̭̪̰͊͆̃̌̅̐͜ ̷̧̼͇̹͙̹̉̐̀̄͠Ÿ̸͔͖̦̱̼́̚̚ͅÓ̸̜̻̊̏̈́̍̓̉̊̔̀̓̽̕͘Ų̵̮͎̙̿̓̽̏̈́̂̊̔͐̓͊̑͂̕͜͜͝Ŗ̵͖͓̳̯͚͇̩̖̙͈̟̙̞̟̥̺́̆ ̷̨̲͖̻̮͇͇͙̜͉͉̥̣͚̹̆̃̎͜͠S̴̝̥̥͚̲͇͓̝͖͓̾͜Ţ̴̥͇̱̮͇̫̳̲̱̙̰̮̯̞̏̊͋̉͐̿̉͐͋͒̇͘À̵̛̛̛̜̇̈́̈́̀̏͌̎͂͘̕̕͝Y̵̡̛͍͓͔̦̦͎͈̳͚̫̥̏̓̑̀̿̆͂̓͛́̐̽͜ͅ]"
-  ];
-  var randomIndex = Math.floor(Math.random() * quotes.length);
-  document.getElementById("hometitleTagline").innerHTML = quotes[randomIndex];
-}
-
